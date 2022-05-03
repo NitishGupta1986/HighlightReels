03b0615acb3d1a6ecc60ecee4bb5a6ca19109f43
Niitishgupta <65271209+Niitishgupta@users.noreply.github.com>
2022-05-03 16:39:21 +0530

    Intial Commit

--- app/src/main/java/com/android/highreels/activities/SplashActivity.kt
+++ app/src/main/java/com/android/highreels/activities/SplashActivity.kt
@@ -1,12 +1,29 @@
 package com.android.highreels.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.android.highreels.R
+import kotlinx.coroutines.*
 
 class SplashActivity : AppCompatActivity() {
+
+    val activityScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_splash)
+        activityScope.launch {
+            delay(3000)
+            var intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
     }
 }
